e81e1f9b68ec5c769213c58ff9983ae7f0b73d06
mohdraz <noorinfotechinc@gmail.com>
2019-10-01 23:06:42 -0500

    First Day Completed

--- src/App.js
+++ src/App.js
@@ -2,9 +2,20 @@ import React from 'react';
 import './App.css';
 import { Home, About, Contact, Navigation } from './components';
 
+// import Home from "./components/Home";
+// import About from "./components/About";
+// import Contact from "./components/Contact";
+
+
+import {Route} from "react-router-dom";
+
+
 const App = () => (
   <div>
     <Navigation />
+    <Route exact path="/" component={Home} />
+    <Route path="/about" component={About} />
+    <Route path="/contact" component={Contact} />
   </div>
 );
 
